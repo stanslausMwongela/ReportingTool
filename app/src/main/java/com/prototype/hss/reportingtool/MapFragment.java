d1e9f4eec58621838c2678a15ef52f039017892e
stanslausMwongela <Stanslaus.Mwongela@strathmore.edu>
2017-05-26 09:49:09 +0300

    git remote add origin https : // github.com/stanslausMwongela/ReportingTool.git git push -u origin master

---
+++ app/src/main/java/com/prototype/hss/reportingtool/MapFragment.java
@@ -0,0 +1,68 @@
+package com.prototype.hss.reportingtool;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements Button.OnClickListener {
+
+    //private OnFragmentInteractionListener mListener;
+    MapView mapView;
+    LatLng position = new LatLng(-1.272262, 36.811752);
+    String markerText = "Prosperity House";
+    private Button mButton; //Add at the top of the fragment
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.map, container, false);
+        //initialize map
+        mapView = (MapView) rootView.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                Log.i("DEBUG", "onMapReady");
+
+                //add marker
+                Marker marker = googleMap.addMarker(new MarkerOptions().position(position).title(markerText));
+
+                //zoom to position with level 16
+                CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(position, 16);
+                googleMap.animateCamera(cameraUpdate);
+            }
+        });
+        return rootView;
+
+
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    public void onClick(View v) {
+        //Nothing here yet
+    }
+
+}
